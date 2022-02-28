42ecf64ff74149a3e9d0520b5fccf18ab5a2f66d
Arda Arslanbakan <ardaarslanbakan@gmail.com>
2022-02-28 09:05:18 +0300

    Highscore is added

--- guess_my_number/script.js
+++ guess_my_number/script.js
@@ -11,8 +11,8 @@ console.log(document.querySelector(".guess").value);
 
 */
 let secretNumber = Math.trunc(Math.random()*20)+1;
-
 let score = 20;
+let highscore = 0;
 document.querySelector(".check").addEventListener("click", function() {
     let guess = Number(document.querySelector(".guess").value);
     console.log(typeof guess);
@@ -24,6 +24,10 @@ document.querySelector(".check").addEventListener("click", function() {
         document.querySelector(".number").style.width="30rem",
         document.querySelector(".message").textContent = "Correct Number";
         score++;
+        if(score> highscore) {
+            highscore = score;
+            document.querySelector(".highscore").textContent = highscore;
+        }
         document.querySelector(".score").textContent = score;
         secretNumber = Math.trunc(Math.random()*20)+1;
         
