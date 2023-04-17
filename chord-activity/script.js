84ee9d021e4e20b504e1833e68aaf721a2db9e22
Arda Arslanbakan <arda.arslanbakan@ug.bilkent.edu.tr>
2023-04-17 10:20:35 +0300

    Update script.js

--- chord-activity/script.js
+++ chord-activity/script.js
@@ -1,8 +1,49 @@
-// Get all the audio-container elements
-const audioContainers = document.querySelectorAll('.audio-container');
+// Select all the draggable audio elements
+const audioElements = document.querySelectorAll('.audio');
 
-// Add a dragstart event listener to each audio-container element
-audioContainers.forEach(audioContainer => {
-  audioContainer.addEventListener('dragstart', e => {
-    // Set the data for the drag event to the ID of the audio-container element
-    e.dataTransfer.setData('text/plain', audioContainer.id
+// Set up event listeners for each audio element
+audioElements.forEach((audioElement) => {
+  // Set the draggable attribute to true
+  audioElement.setAttribute('draggable', true);
+
+  // Add event listeners for the drag events
+  audioElement.addEventListener('dragstart', dragStart);
+  audioElement.addEventListener('dragend', dragEnd);
+});
+
+// Set up event listeners for the drop area
+const dropArea = document.querySelector('.drop-area');
+dropArea.addEventListener('dragover', dragOver);
+dropArea.addEventListener('drop', drop);
+
+// Keep track of the currently dragged element
+let currentlyDraggedElement = null;
+
+function dragStart(event) {
+  // Set the currently dragged element
+  currentlyDraggedElement = event.target;
+
+  // Add the 'dragging' class to the element
+  currentlyDraggedElement.classList.add('dragging');
+}
+
+function dragEnd(event) {
+  // Remove the 'dragging' class from the element
+  currentlyDraggedElement.classList.remove('dragging');
+
+  // Reset the currently dragged element
+  currentlyDraggedElement = null;
+}
+
+function dragOver(event) {
+  // Prevent the default action (which is to not allow drops)
+  event.preventDefault();
+}
+
+function drop(event) {
+  // Prevent the default action (which is to open the audio file in the browser)
+  event.preventDefault();
+
+  // Add the dragged element to the drop area
+  dropArea.appendChild(currentlyDraggedElement);
+}
