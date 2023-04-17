61c0f894a73ab23d6e7d6c1d500536f7998e5b1c
Arda Arslanbakan <arda.arslanbakan@ug.bilkent.edu.tr>
2023-04-17 09:39:50 +0300

    Update script.js

--- chord-activity/script.js
+++ chord-activity/script.js
@@ -19,3 +19,23 @@ containers.forEach(container => {
     draggedContainer = null;
   });
 });
+
+// Get the drop area element
+const dropArea = document.querySelector('#drop-area');
+
+// Add event listeners to the drop area
+dropArea.addEventListener('dragover', handleDragOver);
+dropArea.addEventListener('drop', handleDrop);
+
+// Define the event handlers for the drop area
+function handleDragOver(event) {
+  event.preventDefault();
+  event.dataTransfer.dropEffect = 'move';
+}
+
+function handleDrop(event) {
+  event.preventDefault();
+  const droppedElementId = event.dataTransfer.getData('text/plain');
+  const droppedElement = document.getElementById(droppedElementId);
+  dropArea.insertBefore(droppedElement, dropArea.firstChild);
+}
